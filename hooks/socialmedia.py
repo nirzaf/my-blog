39f9131fb51878e6ff5ffddd7d06fd739d950d86
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-06-14 00:49:16 +0300

    Update social media sharing functionality
    The code for generating the page url and page title for social media sharing has been updated for better readability. Also, a change has been made to ensure the correct formulation of markdown for the social media share buttons.

--- hooks/socialmedia.py
+++ hooks/socialmedia.py
@@ -6,14 +6,15 @@ x_intent = "https://twitter.com/intent/tweet"
 fb_sharer = "https://www.facebook.com/sharer/sharer.php"
 include = re.compile(r"blog/[1-9].*")
 
+
 def on_page_markdown(markdown, **kwargs):
     page = kwargs['page']
     config = kwargs['config']
     if not include.match(page.url):
         return markdown
 
-    page_url = config.site_url+page.url
-    page_title = urllib.parse.quote(page.title+'\n')
+    page_url = config.site_url + page.url
+    page_title = urllib.parse.quote(page.title + '\n')
 
     return markdown + dedent(f"""
     [Share on :simple-x:]({x_intent}?text={page_title}&url={page_url}){{ .md-button }}
